e3312e77009817a92e9aeaa8512397bc09609f4e
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-21 05:46:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,13 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << name
   end
 end
